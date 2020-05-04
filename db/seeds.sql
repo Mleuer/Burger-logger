df257442812d75bec941d7a7a9574ad7fcdbddc9
Matt Leuer <mattleuer@gmail.com>
2020-05-04 16:11:51 -0700

    Initial setup.

---
+++ db/seeds.sql
@@ -0,0 +1,8 @@
+INSERT INTO burgers (burger_name, devoured) 
+VALUES ("Bacon Cheeseburger", false);
+
+INSERT INTO burgers (burger_name, devoured) 
+VALUES ("Breakfast Burger", false);
+
+INSERT INTO burgers (burger_name, devoured) 
+VALUES ("Mushroom and Swiss", true);
